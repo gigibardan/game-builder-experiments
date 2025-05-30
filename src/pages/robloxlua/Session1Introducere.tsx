031f288b97c516168d18c67b134c21165bd99663
mitoconstantIN <constantinmitocaru22@gmail.com>
2025-05-30 15:56:16 +0300

    Rezolvat ruta imagine

--- src/pages/robloxlua/Session1Introducere.tsx
+++ src/pages/robloxlua/Session1Introducere.tsx
@@ -388,7 +388,7 @@ const RobloxLesson1 = () => {
           </InfoBox>
 
           <ImageModal 
-            src="../../assets/images/RobloxL1.4.png" 
+            src="/assets/images/roblox/RobloxL1.4.png" 
             alt="Exemplu de teren finalizat" 
             caption="Exemplu de teren complet, cu texturi și detalii aplicate"
           />
